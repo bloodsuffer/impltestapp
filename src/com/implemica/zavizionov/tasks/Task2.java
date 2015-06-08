44d9bb0716af730925857453eec476eb674a4399
bloodsuffer <monigredo@gmail.com>
2015-06-09 00:28:09 +0300

    Task 2 works with dijkstra algo.

--- src/com/implemica/zavizionov/tasks/Task2.java
+++ src/com/implemica/zavizionov/tasks/Task2.java
@@ -1,31 +1,94 @@
 package com.implemica.zavizionov.tasks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+;
 
 public class Task2 {
-	private int cities = 0;
-	private int tests = 0;
-	private List<List<String>> citiesInTests;
+	// private static int MAX_TESTS = 10;
+	private static int MAX_CITIES = 10000;
+	// private static int MAX_PATHS = 100;
 
-	public static void main(String[] args) {
+	private int nodes = 0;
+	private List<String> cities;
+	private int[][] routeDistances;
 
+	public static void main(String[] args) throws IOException {
+		List<String> input = Files.readAllLines(Paths.get("input.txt"));
+		// TODO
+		input.remove(0);
+		Task2 t = new Task2();
+		t.parse(input);
+		System.out.println(t.cities);
+		System.out.println(Arrays.deepToString(t.routeDistances));
+		System.out.println(Arrays.toString(t.calculateDijkstra(0)));
+		System.out.println(Arrays.toString(t.calculateDijkstra(1)));
 	}
 
 	private void parse(List<String> lines) {
-
 		Iterator<String> it = lines.iterator();
+		nodes = getInt(it.next());
+		if (nodes > MAX_CITIES) {
+			throw new IllegalArgumentException();
+		}
+		cities = new ArrayList<>(nodes);
+		routeDistances = new int[nodes][nodes];
+		for (int i = 0; i < nodes; i++) {
+			Arrays.fill(routeDistances[i], Integer.MAX_VALUE);
+		}
 
-		tests = Integer.parseInt(it.next());
-		cities = Integer.parseInt(it.next());
-		citiesInTests = new ArrayList<>();
-		for (int i = 0; i < tests; i++) {
-			citiesInTests.set(0, new ArrayList<>());
-			List<String> thisCities = citiesInTests.get(i);
-			for (int j = 0; j < cities; j++) {
-				thisCities.set(i, it.next());
+		for (int i = 0; i < nodes; i++) {
+			cities.add(it.next());
+			int numberOfConnections = getInt(it.next());
+			for (int k = 0; k < numberOfConnections; k++) {
+				String str[] = it.next().split(" ");
+				routeDistances[i][getInt(str[0]) - 1] = getInt(str[1]);
 			}
 		}
+		it.next();
+	}
+
+	private Integer getInt(String str) {
+		return Integer.parseInt(str);
+
+	}
+
+	public int[] calculateDijkstra(int cityIndex) {
+		int[] distance = new int[nodes];
+		boolean[] visited = new boolean[nodes];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		distance[cityIndex] = 0;
+
+		while (true) {
+			int current = -1;
+			for (int i = 0; i < nodes; i++) {
+				if (!visited[i] && distance[i] < Integer.MAX_VALUE
+						&& (current == -1 || distance[current] > distance[i])) {
+					current = i;
+				}
+			}
+			if (current == -1) {
+				break;
+			}
+			visited[current] = true;
+			for (int i = 0; i < nodes; i++) {
+				if (!visited[i]
+						&& routeDistances[current][i] < Integer.MAX_VALUE) {
+					distance[i] = Math.min(distance[i], distance[current]
+							+ routeDistances[current][i]);
+				}
+			}
+
+		}
+
+		return distance;
 	}
 }
